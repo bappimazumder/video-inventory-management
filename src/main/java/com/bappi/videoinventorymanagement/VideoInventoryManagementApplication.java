296430e0bca12ed94639af40b5abcba23dedf493
Bappi Mazumder <mazumderbappi@gmail.com>
2024-11-03 16:47:12 +0600

    first commit

---
+++ src/main/java/com/bappi/videoinventorymanagement/VideoInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bappi.videoinventorymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bappi/videoinventorymanagement/VideoInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bappi.videoinventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
